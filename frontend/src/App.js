8b3dc69650321958479df68dff9d22556e51727f
ApoorvaGururajK <kulkarni.apoo@northeastern.edu>
2023-05-10 16:42:15 -0400

    Added the functionality to display images in the image card and delete button functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import Search from './components/Search'
-import {useState } from 'react';
+import {useState } from 'react'
+import ImageCard from './components/ImageCard'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
 
@@ -9,19 +11,20 @@ const App = () =>  {
   const [word,setWord] = useState('')
   const [images, setImages] = useState([])
 
-  console.log(images)
-  
   const handleSearchSubmit = async(e) =>{
     e.preventDefault()
-    console.log(word)
     const response = await fetch(`https://api.unsplash.com//photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
       // .then((res) => res.json())
       // .then((data) => console.log(data))
       // .catch((err) => console.log(err))
       const data = await response.json()
-      setImages([data, ...images])
+      setImages([{...data, title: word}, ...images])
       setWord("")
        
+  }
+
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
   }
   
   // console.log(process.env) //anytime when the state of the component is change, the component is rerendered
@@ -30,6 +33,15 @@ const App = () =>  {
     <div className="App">
       <Header title="Images Gallery"/>
       <Search  setWord={setWord} word={word} handleSubmit={handleSearchSubmit}/>
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard  images={image} handleDelete={handleDeleteImage}/> 
+            </Col> 
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
