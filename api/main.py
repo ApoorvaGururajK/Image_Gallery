5f4c1031df25074926e9310f629e1807a4578765
ApoorvaGururajK <kulkarni.apoo@northeastern.edu>
2023-05-16 01:18:00 -0400

    Enabled debug mode in Flask app

--- api/main.py
+++ api/main.py
@@ -7,11 +7,15 @@ load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
 UNSPLASH_URL = "https://api.unsplash.com//photos/random"
+DEBUG = bool(os.environ.get("DEBUG", True)) # If DEBUG in .env.local has no value, its considered as empty string. And bool("") is False
 
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create a .env.local file and add the unsplash key")
 
 app = Flask(__name__)
+
+# This is done to enable the Debug mode. To disable it, add a  variable called DEBUG in .env.local without value. i.e empty string. DEBUG=
+app.config["DEBUG"] = DEBUG 
 
 @app.route("/new-image")
 def new_image():
