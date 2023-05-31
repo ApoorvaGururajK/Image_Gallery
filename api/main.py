d17344e86dee81778ea982457085ad96becd64fe
ApoorvaGururajK <kulkarni.apoo@northeastern.edu>
2023-05-31 00:18:19 -0400

    Added /images endpoint to make GET and POST call to the database

--- api/main.py
+++ api/main.py
@@ -1,9 +1,12 @@
 import os
 import requests
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from flask_cors import CORS
-from mongodb_client import insert_test_documets
+from mongodb_client import mongo_client
+
+gallery = mongo_client.gallery
+images_collection = gallery.images
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -21,8 +24,6 @@ CORS(app)
 # This is done to enable the Debug mode. To disable it, add a  variable called DEBUG in .env.local without value. i.e empty string. DEBUG=
 app.config["DEBUG"] = DEBUG 
 
-insert_test_documets()
-
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
@@ -37,6 +38,20 @@ def new_image():
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
+
+@app.route("/images", methods=["GET", "POST"])
+def images():
+    if request.method == "GET":
+        # read images from the db
+        images = images_collection.find({})
+        return jsonify([img for img in images])
+    if request.method == "POST":
+        # save image in the db
+        images = request.get_json()
+        images["_id"] = images.get("id")
+        result = images_collection.insert_one(images)
+        inserted_id = result.inserted_id
+        return {"inserted_id": inserted_id}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
